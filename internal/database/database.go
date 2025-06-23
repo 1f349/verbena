0096165973decc9782943682a1064f6c34733d6f
MrMelon54 <github@mrmelon54.com>
2025-06-23 22:31:22 +0100

    Add dbAddr type

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,9 +1,12 @@
 package database
 
 import (
+	"database/sql"
+	"database/sql/driver"
 	"errors"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/mattn/go-sqlite3"
+	"net/netip"
 	"strings"
 	"xorm.io/xorm"
 )
@@ -18,10 +21,34 @@ func New(dbString string) (*xorm.Engine, error) {
 
 type RecordIP struct {
 	Id   int64
-	Name string `xorm:"varchar(25) not null unique 'usr_name' comment('NickName')"`
+	Name string `xorm:"varchar(255) not null 'name'"`
+	Addr dbAddr `xorm:"varchar(40) not null unique 'usr_name' comment('NickName')"` // 40 length is enough for a full IPv6 address 0011:2233:4455:6677:8899:aabb:ccdd:eeff
 }
 
 type RecordTXT struct {
-	Id   int64
-	Name string `xorm:"varchar(25) not null unique 'usr_name' comment('NickName')"`
+	Id    int64
+	Name  string `xorm:"varchar(255) not null 'name'"`
+	Value string `xorm:"varchar(4096) not null 'value'"`
 }
+
+type dbAddr netip.Addr
+
+func (d *dbAddr) Scan(src any) error {
+	str, ok := src.(string)
+	if !ok {
+		return errors.New("invalid IP address")
+	}
+	addr, err := netip.ParseAddr(str)
+	if err != nil {
+		return err
+	}
+	*d = dbAddr(addr)
+	return nil
+}
+
+func (d dbAddr) Value() (driver.Value, error) {
+	return netip.Addr(d).String(), nil
+}
+
+var _ driver.Valuer = dbAddr{}
+var _ sql.Scanner = (*dbAddr)(nil)
